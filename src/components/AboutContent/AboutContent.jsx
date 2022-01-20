805b90ee0bfff2ddef52337ee85fe1a816c82a5a
morshedulmunna <morshedulmunna1@gmail.com>
2022-01-21 02:36:02 +0600

    my skills set update

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -30,60 +30,37 @@ const AboutContent = () => {
             <p>Node.js</p>
             <p>Express.js</p>
             <p>MongoDB</p>
-            <p>Redux</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
+            <p>Redux.js</p>
+            <p>Gatsby.js</p>
+            <p>Tailwind CSS</p>
+            <p>HTML5</p>
+            <p>CSS3</p>
+            <p>PHP</p>
+            <p>MySQL</p>
+            <p>Git</p>
+            <p>Webpack</p>
+            <p>NPM</p>
+            <p>Babel</p>
+            <p>VS Code</p>
+            <p>REST API</p>
+            <p>Firebase</p>
+            <p>C</p>
+            <p>C++</p>
           </div>
           <div className="set2">
             <h3>I want to work with</h3>
             <p>JavaScript</p>
-            <p>ES6</p>
             <p>React.js</p>
-            <p>Node.js</p>
-            <p>Express.js</p>
-            <p>MongoDB</p>
-            <p>Redux</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
+            <p>Gatsby.js</p>
+            <p>REST API</p>
+            <p>Redux.js</p>
+            <p>Tailwind CSS</p>
           </div>
           <div className="set3">
             <h3>I prefer not to work with</h3>
-            <p>JavaScript</p>
-            <p>ES6</p>
-            <p>React.js</p>
-            <p>Node.js</p>
-            <p>Express.js</p>
-            <p>MongoDB</p>
-            <p>Redux</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
-            <p>JavaScript</p>
+            <p>PHP</p>
+            <p>Laravel</p>
+            <p>JQuery</p>
           </div>
         </div>
       </div>
