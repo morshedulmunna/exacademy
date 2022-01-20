1a037005a31dc7955928e1bd172589f87627f960
morshedulmunna <morshedulmunna1@gmail.com>
2022-01-20 19:24:27 +0600

    info details

--- src/Route/Home.js
+++ src/Route/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import InfoDetails from "../components/InfoDetails/InfoDetails.jsx";
 import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <InfoDetails />
     </div>
   );
 };
