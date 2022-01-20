e851f630cc05efe3262019932f89f954299d075a
morshedulmunna <morshedulmunna1@gmail.com>
2022-01-21 02:15:13 +0600

    about page content added

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -1,11 +1,91 @@
 import React from "react";
 import "./AboutContent.css";
+import H_Image from "../../../src/images/infosecimg.png";
 
 const AboutContent = () => {
   return (
     <>
-      <div className="div">
-        <h2>Thi Is About</h2>
+      <div className="container about__content">
+        <div className="programmer__info">
+          <img src={H_Image} alt="" />
+          <div className="programmertext__area">
+            <h2>
+              <span>I'm</span> Morshedul Munna
+            </h2>
+            <p>
+              A self-motivated and enthusiastic web developer with a deep
+              interest in JavaScript. To work in the Software industry with
+              modern web technologies of different local & multinational
+              Software/ IT agencies of Bangladesh and grow rapidly with
+              increasing responsibilities.
+            </p>
+          </div>
+        </div>
+        <div className="skills__set">
+          <div className="set1">
+            <h3>My Skills Set</h3>
+            <p>JavaScript</p>
+            <p>ES6</p>
+            <p>React.js</p>
+            <p>Node.js</p>
+            <p>Express.js</p>
+            <p>MongoDB</p>
+            <p>Redux</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+          </div>
+          <div className="set2">
+            <h3>I want to work with</h3>
+            <p>JavaScript</p>
+            <p>ES6</p>
+            <p>React.js</p>
+            <p>Node.js</p>
+            <p>Express.js</p>
+            <p>MongoDB</p>
+            <p>Redux</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+          </div>
+          <div className="set3">
+            <h3>I prefer not to work with</h3>
+            <p>JavaScript</p>
+            <p>ES6</p>
+            <p>React.js</p>
+            <p>Node.js</p>
+            <p>Express.js</p>
+            <p>MongoDB</p>
+            <p>Redux</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+            <p>JavaScript</p>
+          </div>
+        </div>
       </div>
     </>
   );
