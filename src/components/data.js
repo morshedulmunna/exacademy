d0a992f8941611b4dbb2605c5a07f91fc83bb214
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-03 01:09:31 +0600

    Data Create

--- src/components/data.js
+++ src/components/data.js
@@ -1,8 +1,41 @@
-export const HomeData = {
-  topLine: "Hello I'm",
-  name: "Morshedul Munna",
-  profession1: "Web Developer",
-  profession2: "Programmer",
-  discription:
-    "A self-motivated and enthusiastic web developer with a deep interest in JavaScript. To work in the Software industry with modern web technologies of different local & multinational Software/ IT agencies of Bangladesh and grow rapidly with increasing responsibilities.",
-};
+const data = [
+  {
+    id: 1,
+    name: "Red Onion Food",
+    discription:
+      "A single page restaurant app where people are able to select food in their cart and checkout the cart with their information .",
+    image: "https://i.ibb.co/QKyZJFc/X3h3ei5.png",
+    tecnology: ["React.js", "firebase", "Node.js", "Express.js", "Bootstrap-5"],
+  },
+  {
+    id: 2,
+    name: "Doctor's Portal",
+    discription:
+      "A Single page web app with dashboard where people can pick the date and time and fix an appointment. A doctor can review his appointments and prescribe medicine to a Patient from his dashboard.",
+    image: "https://i.ibb.co/Lp2vJpJ/fxP7ERu.png",
+    tecnology: [
+      "React.js",
+      "firebase",
+      "Node.js",
+      "Express.js",
+      "Bootstrap-5",
+      "MongoDB",
+      "Nodemon",
+    ],
+  },
+  {
+    id: 3,
+    name: "ExamHelpBD",
+    discription:
+      "ExamHelpBD is the Edtech platform for Teachers and Students. Any teacher can post notes and questions here. Students can read and practice tests from anywhere and anytime.",
+    image: "https://i.ibb.co/bm5zftz/5pI9nyI.png",
+    tecnology: [
+      "React.js",
+      "firebase",
+      "Node.js",
+      "Express.js",
+      "Bootstrap-5",
+      "JavaScript",
+    ],
+  },
+];
