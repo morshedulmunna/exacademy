e2f4f113057dac6aad694b9d016a2d6156a6d427
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-03 11:02:40 +0600

    All Work btn added

--- src/SomeWork/SomeWork.jsx
+++ src/SomeWork/SomeWork.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./SomeWork.css";
+import { Link } from "react-router-dom";
 
 const SomeWork = () => {
-  return <section className=" container wrapper"></section>;
+  return (
+    <section className="container">
+      <div className="someWork">
+        <h3>Some of my Works</h3>
+        <div className="allWork">
+          <span></span>
+          <Link to="/work">All Works</Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SomeWork;
