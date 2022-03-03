32cbebc91ec569eac04b4256e35b7169e4bed9f4
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-03 14:51:30 +0600

    work Show

--- src/SomeWork/SomeWork.jsx
+++ src/SomeWork/SomeWork.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./SomeWork.css";
 import { Link } from "react-router-dom";
+import data from "../data.js";
+
+// eslint-disable-next-line array-callback-return
+data.map((work) => {
+  console.log(work);
+});
 
 const SomeWork = () => {
   return (
@@ -17,7 +23,16 @@ const SomeWork = () => {
           <img src="https://imgur.com/X3h3ei5.png" alt="" />
         </div>
         <div className="workContent">
-          <p>Content122</p>
+          <span>01</span>
+          <h2>Red Onion Foods</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            incidunt cupiditate praesentium dolores consectetur consequuntur
+            animi voluptatum aspernatur deleniti alias!
+          </p>
+          <div className="skills__set">
+            <p>JavaScript</p>/
+          </div>
         </div>
       </div>
     </section>
