b122585aaecc24453aa3d431dd1ec577fed50709
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-17 20:18:29 +0600

    new ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-
-ReactDOM.render(
-  <BrowserRouter>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
